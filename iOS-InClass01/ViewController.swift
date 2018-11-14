7da07a74423b0461d5a6b71c4d6657d4a0aef859
Alay Chaniyara <alaychaniyara@gmail.com>
2018-11-13 23:01:38 -0500

    iOS-Inclass 1 to understand view controller and getting and setting data into the view and creating constrainted layouts. This simple calculator app lets you understand this.

--- iOS-InClass01/ViewController.swift
+++ iOS-InClass01/ViewController.swift
@@ -7,14 +7,115 @@
 //
 
 import UIKit
+import Toast_Swift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var results: UILabel!
+    @IBOutlet weak var buttonAdd: UIButton!
+    @IBOutlet weak var buttonSubtract: UIButton!
+    @IBOutlet weak var buttonMultiply: UIButton!
+    @IBOutlet weak var buttonDivide: UIButton!
+//    var characters = ""
+//    var isNumeric: Bool {
+//      
+//        let nums: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+//        return Set(self.characters).isSubset(of: nums)
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func Add(_ sender: Any) {
+    
+        let fno = Double( firstNumber.text!)
+        let sno = Double( secondNumber.text!)
 
+        
+        if((firstNumber.text?.isEmpty)! || (secondNumber.text?.isEmpty)!)
+        {
+            
+            self.view.makeToast("Input Cannot be Empty")
+            results.text = "Input cannot be empty"
+        }
+        else
+        {
+            let sum =  fno! + sno!
+            
+            results.text = "Results : \(sum)"
+        }
+        
+        
+        
+    }
+    
+    @IBAction func Subtract(_ sender: Any) {
+        let fno = Double( firstNumber.text!)
+        let sno = Double( secondNumber.text!)
+        
+        if((firstNumber.text?.isEmpty)! || (secondNumber.text?.isEmpty)!)
+        {
+            self.view.makeToast("Input Cannot be Empty")
+            
+                 results.text = "Input cannot be empty"
+        }
+        else
+        {
+            let minus =  fno! - sno!
+            
+            results.text = "Results : \(minus)"
+        }
+    }
+    @IBAction func Multiply(_ sender: Any) {
+        let fno = Double( firstNumber.text!)
+        let sno = Double( secondNumber.text!)
+        
+        if((firstNumber.text?.isEmpty)! || (secondNumber.text?.isEmpty)!)
+        {
+            self.view.makeToast("Input Cannot be Empty")
+            
+            results.text = "Input cannot be empty"
+        }
+        else
+        {
+            let multiply =  fno! * sno!
+            
+            results.text = "Results : \(multiply)"
+        }
+    }
+    @IBAction func Clear(_ sender: Any) {
+        results.text = "Results : 0.00"
+        firstNumber.text = ""
+        secondNumber.text = ""
+    }
+    @IBAction func Divide(_ sender: Any) {
+        let fno = Double( firstNumber.text!)
+        let sno = Double( secondNumber.text!)
+        
+        if((firstNumber.text?.isEmpty)! || (secondNumber.text?.isEmpty)!)
+        {
+            self.view.makeToast("Input Cannot be Empty")
+            
+            results.text = "Input cannot be empty"
+        }
+        else  if(sno! == 0){
+            //results.text = "Divide by zero error"
+            let alertController = UIAlertController(title: "Error", message: "Divide by zero error", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: "Default Action"), style: .default, handler: nil))
+            self.present(alertController, animated: true)
+        }
+        else
+        {
+            let divide =  fno! / sno!
+            
+            results.text = "Results : \(divide)"
+        }
+    }
 }
 
